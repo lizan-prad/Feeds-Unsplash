7531f025ddbba5404b3ada29aa983f0207b7cf78
Lizan <lizan@Lizans-MacBook-Pro.local>
2024-06-17 14:28:21 +0400

    clientId set to

--- TeletronincsAssessment/Helpers/Configuration.swift
+++ TeletronincsAssessment/Helpers/Configuration.swift
@@ -12,7 +12,7 @@ class Configuration {
     static var clientID: String {
         // Retrieve the `ClientID` value from Info.plist
         // FYI: Use the `ClientID-2` incase this key exceeds limit
-        if let clientID = Bundle.main.object(forInfoDictionaryKey: "ClientID") as? String {
+        if let clientID = Bundle.main.object(forInfoDictionaryKey: "ClientID-2") as? String {
             return clientID
         } else {
             return ""
